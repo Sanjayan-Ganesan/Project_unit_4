3437e87e4711caf7d8e10223851d68e6555715b9
Sanjayan <97502651+Sanjayan-Ganesan@users.noreply.github.com>
2022-05-08 23:37:21 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Title from './Component/Title';
+import Main from './Component/Main';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
